9483a874af0915da415b732eba21480ffbf007b5
ana <ana@anniecannons.com>
2022-12-11 14:51:57 -0800

    added the helper folder and the router folder

---
+++ Develop/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /notes 
+const notes = require('./notes');
+
+
+const app = express();
+
+app.use('/notes', notes);
+
+
+module.exports = app;
---
+++ Develop/routes/notes.js
@@ -0,0 +1,44 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title , text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+     title,
+     text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, '.../db/db.json');
+    res.json(`a Note has been added successfully 🚀`);
+  } else {
+    res.error('Error in adding a note ');
+  }
+
+  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+  app.delete('/api/notes/:id', (req, res) => {
+    // reading notes form db.json
+    let db = JSON.parse(fs.readFileSync('..db/db.json'))
+    // removing note with id
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+    // Rewriting note to db.json
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+    
+  })
+
+
+});
+
+module.exports = notes;
