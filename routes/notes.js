c530aa6e8870b82ac65a02522010aeac04065276
ana <ana@anniecannons.com>
2022-12-13 16:32:52 -0800

    added the delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,8 @@
 const notes = require("express").Router();
-const express = require('express');
+const fs = require('fs');
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
-const app = express()
+
 // GET Route for retrieving all the notes
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
@@ -26,24 +26,19 @@ notes.post("/", (req, res) => {
   } else {
     res.err("Error in adding a note ");
   }
-
-  
-  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
-  app.delete("/api/notes/:id", (req, res) => {
-    // reading notes form db.json
-    let db = JSON.parse(fs.readFileSync("./db/db.json"));
-    // removing note with id
-    let deleteNotes = db.filter((item) => item.id !== req.params.id);
-    // Rewriting note to db.json
-    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNotes));
-    res.json(deleteNotes);
-  });
-
-
-
-
-
-
 });
+
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+notes.delete("/:id", (req, res) => {
+  console.log("HIT");
+  // reading notes form db.json
+  let db = JSON.parse(fs.readFileSync("./db/db.json"));
+  // removing note with id
+  let deleteNotes = db.filter((item) => item.id !== req.params.id);
+  // Rewriting note to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
+});
+
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -19,18 +19,18 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 
 // GET Route for wild card
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
